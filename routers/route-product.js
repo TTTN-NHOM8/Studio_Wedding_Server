c9a42b80f700ef0d0d1d13a45c94581193f6cceb
dyubudci <duykap123@gmail,com>
2023-11-25 10:45:08 +0700

    fix code server route

--- routers/route-product.js
+++ routers/route-product.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const product=require('../controllers/product-controller');
 
 router.get('/product',product.getProduct);            
-router.get('/product/:idHopDong',product.getProductById);               
+router.get('/product/:idSanPham',product.getProductById);               
 router.get('/product/payment/:trangThai',product.getProductByPayment);
 router.put('/product/delete/:idSanPham',product.deleteProduct);
 
