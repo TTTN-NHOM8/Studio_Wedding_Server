0cfd92a8159e2ce7f30d1c49af2857f92513a775
Tys7603 <long.vlgt@gmail.com>
2023-11-25 09:03:55 +0700

    LongNX - api task - 25-11-2023

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -24,7 +24,44 @@ const readTaskByRole = async (req, res) => {
     }
 }
 
+const updateTask = async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        const { statusTask } = req.body
+
+        if (!id || !statusTask) {
+            return res.status(400).json({ status: "error", message: "parameter is missing or empty." });
+        }
+
+        const results = await model.updateTask(id, statusTask)
+        res.json({ status: "success", results })
+    } catch (error) {
+        res.status(500).json({ status: "error", error: error.message });
+    }
+
+}
+
+
+const deleteTask = async (req, res) => {
+
+    try {
+        const id = req.params.id;
+
+        if (!id) {
+            return res.status(400).json({ status: "error", message: "parameter is missing or empty." });
+        }
+
+        const results = await model.deleteTask(id)
+        res.json({ status: "success", results })
+    } catch (error) {
+        res.status(500).json({ status: "error", error: error.message });
+    }
+
+}
 module.exports = {
     readTask,
-    readTaskByRole
+    readTaskByRole,
+    updateTask,
+    deleteTask
 }
