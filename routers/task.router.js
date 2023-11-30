03737265a6cccbdff8e6e29c05531924bcda3fc4
Tys7603 <long.vlgt@gmail.com>
2023-11-30 07:20:48 +0700

    LongNX - api task - 30/11/2023

--- routers/task.router.js
+++ routers/task.router.js
@@ -3,7 +3,14 @@ const controller = require('../controllers/task.controller.js')
 
 router.get("/tasks", controller.readTask)
 router.get("/tasks/role", controller.readTaskByRole)
+router.get("/employees/:idHDCT", controller.readEmployeeByIdHDCT)
+
+router.post("/role/employees", controller.readEmployeeByRole)
+router.post("/insert/employee", controller.insertEmployeeJoin)
+
 router.put("/update/task/:id", controller.updateTask)
+
 router.delete("/delete/task/:id", controller.deleteTask)
+router.delete("/delete/employee/:idJoin", controller.deleteJoin)
 
 module.exports = router
