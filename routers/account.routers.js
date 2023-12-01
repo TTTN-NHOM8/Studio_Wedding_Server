08035e136c296f28deff34ac69b726c33f7b414b
2023thong <thongpdpd06029@fpt.edu.vn>
2023-12-01 16:36:11 +0700

    Thong_Update_01/12/2023

--- routers/account.routers.js
+++ routers/account.routers.js
@@ -1,6 +1,27 @@
-const router = require('express').Router();
-const accountControllers = require('../controllers/account.controllers.js');
+const router = require("express").Router();
+const accountControllers = require("../controllers/account.controllers.js");
 
-router.post('/account/Login', accountControllers.LoginAccount);
+router.post("/account/Login", accountControllers.LoginAccount);
+router.post(
+  "/account/changePassword",
+  accountControllers.ChangePasswordController
+);
+router.post("/account/update", accountControllers.UpdateEmployeeInfoController);
+router.get(
+  "/account/get-updated-info/:idNhanVien",
+  accountControllers.Getthongtin
+);
+router.get(
+  "/getDailyRevenue/:ngay",
+  accountControllers.GetDailyRevenueController
+);
+router.get(
+  "/getDailyRevenueMonth/:thang",
+  accountControllers.GetDailyRevenueControllerMonth
+);
+router.get(
+  "/getDailyRevenueYert/:nam",
+  accountControllers.GetDailyRevenueControllerYert
+);
 
 module.exports = router;
